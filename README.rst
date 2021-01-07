72a38c5d79a0dfc811cff50d9d2ae67994985816
Adar Nimrod <nimrod@shore.co.il>
2021-01-07 21:59:24 +0200

    Updates.
    - GitLab CI.
    - Refresh pre-commit checks.
    - Address some issues.

--- README.rst
+++ README.rst
@@ -1,8 +1,9 @@
 Python pre-commit hooks
 #######################
 
-.. image:: https://travis-ci.org/adarnimrod/python-pre-commit.svg?branch=master
-    :target: https://travis-ci.org/adarnimrod/python-pre-commit
+.. image:: https://git.shore.co.il/nimrod/python-pre-commit/badges/master/pipeline.svg
+    :target: https://git.shore.co.il/nimrod/python-pre-commit/-/commits/master
+    :alt: pipeline status
 
 Ansible `pre-commit <http://pre-commit.com/>`_ hooks.
 
@@ -16,9 +17,9 @@ Add the following to your :code:`.pre-commit-config.yaml`:
 .. code:: yaml
 
     - repo: https://git.shore.co.il/nimrod/python-pre-commit.git
-      sha: v0.2.0
+      rev: v0.2.0
       hooks:
-      - id: piprot
+        - id: piprot
 
 License
 -------
@@ -32,4 +33,4 @@ Author Information
 Nimrod Adar, `contact me <nimrod@shore.co.il>`_ or visit my `website
 <https://www.shore.co.il/>`_. Patches are welcome via `git send-email
 <http://git-scm.com/book/en/v2/Git-Commands-Email>`_. The repository is located
-at: https://git.shore.co.il/nimrod/.
+at: https://git.shore.co.il/explore.
--- setup.py
+++ setup.py
@@ -2,7 +2,28 @@ from setuptools import setup
 
 
 setup(
-        name='pre_commit_python_dummy_package',
-        version=open('VERSION', 'r').read().strip(),
-        install_requires=['piprot', 'docutils'],
+    name="pre_commit_python_dummy_package",
+    version=open("VERSION", "r").read().strip(),
+    install_requires=["piprot", "docutils"],
+    description="Dummy package for installing with pre-commit.",
+    long_description=open("README.rst", "r").read(),
+    long_description_content_type="text/x-rst",
+    url="https://git.shore.co.il/nimrod/python-pre-commit",
+    author="Nimrod Adar",
+    author_email="nimrod@shore.co.il",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="pre-commit",
 )
